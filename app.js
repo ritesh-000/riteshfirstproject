7822c095caa3bcc84be1eec8856a7b5b368ca4b8
ritesh-000 <ritesh972085@gmail.com>
2026-03-30 09:42:41 -0700

    fixed port issue for render

--- app.js
+++ app.js
@@ -113,6 +113,11 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render("error.ejs",{message})
 //    res.status(statusCode).send(message);
 })
-app.listen(8080,()=>{
-    console.log("Server is listening on port 8080");
+// app.listen(8080,()=>{
+//     console.log("Server is listening on port 8080");
+// });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
